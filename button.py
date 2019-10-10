0c88a55ae2f36a69c1266985e99b231f1f2dc6ff
Bryan <tw_bryan_chen@intretech.com>
2019-10-10 17:30:01 +0800

    Update button.py

--- button.py
+++ button.py
@@ -1,15 +1,24 @@
 from tkinter import *
+import time
+ 
 def msgShow():
-    label["text"]="文字中文顯示"
-    label["bg"]="lightyellow"
-    label["fg"]="blue"
+    label1 = Label(window, text="Label 1 中文顯示", 
+              bg="lightyellow", 
+              width =15, 
+              font="Helvetica 28 bold italic") 
+    label1.pack(side=TOP)          
+    #time.sleep(3)
+    window.after(3000, window.destroy)
+   
 
 window = Tk()
-window.title("Button Example")    
-label = Label(window)
-btn = Button(window, text="Show Message",command=msgShow)
+window.title("Button Example")  
+window.geometry("800x600") 
+#label = Label(window)
+btn1 = Button(window, text="Show Message",width=15, command=msgShow)
+btn2 = Button(window, text="Exit",width=15, command=window.destroy)
 
-label.pack()
-btn.pack()
-
+#label.pack()
+btn1.pack(side=LEFT)
+btn2.pack(side=LEFT)
 window.mainloop()
