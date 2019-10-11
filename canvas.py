7014473dcdc67da9ab28106ffff62b9cbf1299c4
Bryan <tw_bryan_chen@intretech.com>
2019-10-11 12:12:11 +0800

    Create canvas.py

---
+++ canvas.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import cv2
+# Create a window
+tk = Tk()
+# Load an image using OpenCV
+cv_img = cv2.cvtColor(cv2.imread("dog.jpg"), cv2.COLOR_BGR2RGB)
+# Get the image dimensions 
+height, width, channels = cv_img.shape
+# Create a canvas that can fit the above image
+canvas = Canvas(tk, width = width+40 , height = height+30, bg="grey90")
+#canvas = Canvas(tk, width=800, height=600, bg="yellow")
+# Use PIL (Pillow) to convert the NumPy ndarray to a PhotoImage
+photo = ImageTk.PhotoImage(image = Image.fromarray(cv_img))
+# Add a PhotoImage to the Canvas
+canvas.create_image(20, 15, image=photo, anchor=NW) #NW North West
+canvas.pack()
+tk.mainloop()
+
