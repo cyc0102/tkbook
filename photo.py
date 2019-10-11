391b279dc20155318713fedc642c97fd7ebe332d
Bryan <tw_bryan_chen@intretech.com>
2019-10-11 10:52:45 +0800

    update

--- photo.py
+++ photo.py
@@ -5,6 +5,7 @@ window.title("Show Photo")
 window.geometry("800x600")
 
 photo_gif = PhotoImage(file="dog.gif")
-Label(window, image=photo_gif).pack()
-
+Label(window, image=photo_gif).pack(side="left")
+photo_png = PhotoImage(file="dog.png")
+Label(window, image=photo_png).pack(side="right")
 window.mainloop()
